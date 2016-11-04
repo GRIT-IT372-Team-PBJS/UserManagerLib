c5617cf0c9a14d108a2db52c3d0af08338e49d6c
Peter Kim <Peter Kim>
2016-11-03 22:35:15 -0700

    Started working on the registration class

---
+++ LoginLib/Registration.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 11/3/2016
+ * Time: 7:32 PM
+ */
+
+require_once "User.php";
+require_once "Database.php";
+
+class Registration
+{
+    private $db;
+    private $registeredUser;
+
+    public static function registerUser($firstName, $middleName, $lastName, $email, $registeredSite, $type){
+        if(self::userExists($email)){
+            self::registerUserToSite($email, $registeredSite);
+        } else {
+            self::addNewUserToDB($firstName, $middleName, $lastName, $email, $registeredSite, $type);
+            self::getUserId();
+        }
+    }
+
+    private static function userExists($email) {
+
+
+
+    }
+
+    private static function registerUserToSite($email, $registeredSite) {
+
+    }
+
+    private static function getUserId() {
+
+    }
+
+    private static function addNewUserToDB($firstName, $middleName, $lastName, $email, $registeredSite, $type) {
+
+    }
+}
