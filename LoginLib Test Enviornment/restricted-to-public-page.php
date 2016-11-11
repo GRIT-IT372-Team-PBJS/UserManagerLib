47de5c08132e37f3d13632e8a808edeb44777ba2
Peter Kim <PeterLKim1337@gmail.com>
2016-11-11 02:56:34 -0800

    added password change functionality

--- LoginLib Test Enviornment/restricted-to-public-page.php
+++ LoginLib Test Enviornment/restricted-to-public-page.php
@@ -2,7 +2,31 @@
     require_once "../LoginLib/Authentication.php";
     session_start();
 
-    Authentication::isValidUserElseRedirectTo("https://www.google.com");
-    echo $_SESSION["auth-current-user"]->getFirstName();
+    Authentication::isValidUserElseRedirectTo("login-page.php");
+
+    if(isset($_POST["logout"])){
+        echo "hello";
+        Authentication::logout("login-page.php");
+    }
+
+    if(isset($_POST["submit2"])){
+
+       Authentication::changePassword($_POST["newPassword"]);
+    }
+
 ?>
 HEoLLO WORLD
+
+<form method="post" action="">
+
+    <input type="submit" name="logout" value="Logout">
+</form>
+
+<form method="post" action="">
+    <h3>Change Password</h3>
+
+    <input type="text" name="newPassword">
+    <input type="submit" name="submit2">
+
+</form>
+
