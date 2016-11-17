51046e2a88d816240ca5547b617f34f588e11a72
Peter Kim <PeterLKim1337@gmail.com>
2016-11-17 02:59:46 -0800

    updated the database so uploaded the new database sql file

--- artifacts/loginlib.sql
+++ artifacts/loginlib.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 09, 2016 at 05:39 AM
+-- Generation Time: Nov 17, 2016 at 11:58 AM
 -- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.23
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `loginlib`
 --
+CREATE DATABASE IF NOT EXISTS `loginlib` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `loginlib`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth`
+--
+
+CREATE TABLE IF NOT EXISTS `auth` (
+  `auth_id` int(11) NOT NULL AUTO_INCREMENT,
+  `auth_type` varchar(255) NOT NULL,
+  `auth_rank` int(11) NOT NULL,
+  PRIMARY KEY (`auth_id`),
+  UNIQUE KEY `auth_type` (`auth_type`),
+  UNIQUE KEY `auth_rank` (`auth_rank`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `auth`
+--
+
+INSERT INTO `auth` (`auth_id`, `auth_type`, `auth_rank`) VALUES
+(1, 'SUPER_ADMIN', 0),
+(2, 'ADMIN', 1),
+(3, 'REGULAR', 10);
 
 -- --------------------------------------------------------
 
@@ -26,10 +52,18 @@ SET time_zone = "+00:00";
 -- Table structure for table `sites`
 --
 
-CREATE TABLE `sites` (
+CREATE TABLE IF NOT EXISTS `sites` (
   `site_id` int(11) NOT NULL,
-  `site_name` varchar(255) NOT NULL
+  `site_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`site_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sites`
+--
+
+INSERT INTO `sites` (`site_id`, `site_name`) VALUES
+(1, 'it-connect');
 
 -- --------------------------------------------------------
 
@@ -37,14 +71,43 @@ CREATE TABLE `sites` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `user_id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(125) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `middename` varchar(255) DEFAULT NULL,
-  `password` varchar(255) NOT NULL
+  `auth_type` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `firstname`, `lastname`, `middename`, `auth_type`, `password`) VALUES
+(1, 'pk@gmail.com', 'Peter', 'Kim', 'Lee', '', '$2y$10$JlqUPqGe4Q4/evjzjQTrvOo1E4iceSPyVRwlcVu/W06zLhMDaQQCy'),
+(3, 'pk2@gmail.com', 'Peter', 'Kim', 'Lee', 'SUPER_ADMIN', '$2y$10$ZBMFVQjGCNCTpDpmmssDterHr43rlKuNOaFdqCkyVe68ki9yq8jAi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_auth_xref`
+--
+
+CREATE TABLE IF NOT EXISTS `user_auth_xref` (
+  `user_id` int(11) NOT NULL,
+  `auth_type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_auth_xref`
+--
+
+INSERT INTO `user_auth_xref` (`user_id`, `auth_type`) VALUES
+(3, 'SUPER_ADMIN');
 
 -- --------------------------------------------------------
 
@@ -52,43 +115,21 @@ CREATE TABLE `users` (
 -- Table structure for table `user_site_xref`
 --
 
-CREATE TABLE `user_site_xref` (
+CREATE TABLE IF NOT EXISTS `user_site_xref` (
   `user_id` int(11) NOT NULL,
-  `site_id` int(11) NOT NULL
+  `site_id` int(11) NOT NULL,
+  KEY `user_id` (`user_id`),
+  KEY `site_id` (`site_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `user_site_xref`
 --
 
---
--- Indexes for table `sites`
---
-ALTER TABLE `sites`
-  ADD PRIMARY KEY (`site_id`);
+INSERT INTO `user_site_xref` (`user_id`, `site_id`) VALUES
+(1, 1),
+(3, 1);
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- Indexes for table `user_site_xref`
---
-ALTER TABLE `user_site_xref`
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `site_id` (`site_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
